3687c7478002e3fc2ad6c49cfe486310c04585b1
Greg Bulmash <burgerguy@gmail.com>
2022-02-19 17:14:09 -0800

    more scaffolding

---
+++ modules/stats-store/stats.js
@@ -0,0 +1,9 @@
+/**********************************************
+ *  GH-StatsBot - A GitHub Analytics Tool
+ * 
+ *  Copyright ©2022 Greg Bulmash
+ *  Licensed under MIT License
+ * 
+ *  This file is the stats storage module
+ * 
+ *********************************************/
